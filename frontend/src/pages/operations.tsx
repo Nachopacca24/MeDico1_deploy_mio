9e14d01ab2b3e00128c7386fdad2932d62abb1ad
Tu Nombre <josepaccagnella@ufm.edu>
2026-06-05 07:21:18 -0600

    arrelgo de loas cirguas

--- frontend/src/pages/operations.tsx
+++ frontend/src/pages/operations.tsx
@@ -53,7 +53,7 @@ const folderStructure = {
     "Obstetricia": "Obstetricia/Obstetricia.csv",
   },
   Oftalmología: {
-    "Oftalmología": "Oftamología/Oftalmología.csv",
+    "Oftalmología": "Oftalmología/Oftalmología.csv",
   },
   Ortopedia: {
     "Cadera": "Ortopedia/Cadera.csv",
@@ -80,7 +80,7 @@ const folderStructure = {
     "Uñas y Piel": "Procesos_variados/Uñas___piel.csv",
   },
   Urología: {
-    "Urología": "Urologia/Urología.csv",
+    "Urología": "Urología/Urología.csv",
   },
 };
 
